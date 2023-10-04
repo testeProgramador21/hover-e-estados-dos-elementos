390dba56673832ad8b3fe33d5b0084dd79fd0205
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-04 03:15:35 -0300

    completo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ const Page = () => {
   return (
     <div className="container mx-auto">
 
+      <input type="text" className="w-full h-7 border-2 border-black m-3 focus:bg-green-200" />
       
+      <button className="bg-red-200 p-4 hover:bg-blue-200">Clique aqui</button>
     </div>
   );
 }
